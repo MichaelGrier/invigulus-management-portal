857d195211967dbe518ba175e1c80a18f4682c89
ajoubert655 <drieslj@gmail.com>
2020-11-18 18:54:41 -0700

    UserPage appearance mostly complete

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 //import LandingPage from './containers/LandingPage/LandingPage';
 //import ManageOrgs from './containers/ManageOrgs/ManageOrgs';
-import LoginPage from './containers/LoginPage/LoginPage';
+//import LoginPage from './containers/LoginPage/LoginPage';
+import UserPage from './containers/UserPage/UserPage';
 
 
 
@@ -11,7 +12,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <LoginPage/>
+        <UserPage/>
+        {/*<LoginPage/>*/}
         {/*<ManageOrgs />*/}
       </div>
     );
