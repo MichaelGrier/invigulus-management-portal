ce3b3ca91989b1b8cb5cad405ab0cf7b4bad7296
Michael Grier <michael.c.grier@gmail.com>
2020-11-19 08:28:57 -0700

    format app.js for merge

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
 
-//import LandingPage from './containers/LandingPage/LandingPage';
-//import ManageOrgs from './containers/ManageOrgs/ManageOrgs';
-//import LoginPage from './containers/LoginPage/LoginPage';
-import UserPage from './containers/UserPage/UserPage';
-
-
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <UserPage/>
-        {/*<LoginPage/>*/}
-        {/*<ManageOrgs />*/}
+
       </div>
     );
   }
